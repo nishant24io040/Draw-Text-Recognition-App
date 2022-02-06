04d9a02973fa323c1d0cf251162bfe2ad3ae974d
Nishant Goswami <nishant24io040@satiengg.in>
2022-02-07 05:12:54 +0530

    version 1.0.0

--- app/src/main/java/com/example/drawingapp/MainActivity.java
+++ app/src/main/java/com/example/drawingapp/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.drawingapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowInsetsControllerCompat;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.SparseArray;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,7 +36,9 @@ import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     PaintView drawableView;
+    AlertDialog.Builder alertDialog;
     FloatingActionButton undo,colorpicker,clear,redo;
+    Button button;
 //    TextView text;
 
     @Override
@@ -40,11 +47,13 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
         undo = findViewById(R.id.undo);
         colorpicker = findViewById(R.id.colorpicker);
         clear = findViewById(R.id.clear);
         redo = findViewById(R.id.redo);
         drawableView = findViewById(R.id.drawableView);
+        alertDialog = new AlertDialog.Builder(MainActivity.this);
 //        text=findViewById(R.id.textView);
 
        drawableView.setBrushSize(10);
@@ -63,6 +72,44 @@ public class MainActivity extends AppCompatActivity {
         undo.setOnClickListener(view -> drawableView.undoDrawing());
         clear.setOnClickListener(view -> drawableView.clearCanvas());
         redo.setOnClickListener(view -> drawableView.redoDrawing());
+
+        button.setOnClickListener(view -> {
+            View view1 = getLayoutInflater().inflate(R.layout.dialog,null);
+            final EditText copableText = view1.findViewById(R.id.editText);
+            TextView btnCancel = view1.findViewById(R.id.close);
+            TextView btnCopy =view1.findViewById(R.id.copy);
+            alertDialog.setView(view1);
+
+            Bitmap bitmap = drawableView.getCanvasBitmap();
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+            if(!textRecognizer.isOperational()){
+                Toast.makeText(this, "Could not get text", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                StringBuilder sb = new StringBuilder();
+                for (int i=0;i<items.size();i++){
+                    TextBlock myitem = items.valueAt(i);
+                    sb.append(myitem.getValue());
+                }
+                copableText.setText(sb.toString());
+            }
+
+            final AlertDialog alert = alertDialog.create();
+            alert.setCanceledOnTouchOutside(true);
+            btnCancel.setOnClickListener(view2 -> {
+                alert.dismiss();
+            });
+            btnCopy.setOnClickListener(view3 -> {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("dataTobeCopy", copableText.getText().toString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(this, "Copied", Toast.LENGTH_SHORT).show();
+            });
+            alert.show();
+        });
+
 //
 //        save.setOnClickListener(view -> {
 //            Bitmap bitmap = drawableView.getCanvasBitmap();
---
+++ app/src/main/java/com/example/drawingapp/SpashScreen.java
@@ -0,0 +1,36 @@
+package com.example.drawingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SpashScreen extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+        imageView = findViewById(R.id.imageView);
+        Glide.with(SpashScreen.this).load(R.drawable.resource_final).centerCrop().into(imageView);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SpashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+}
